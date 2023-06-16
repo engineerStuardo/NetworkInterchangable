5d47f2b2152b2b1243f43c0a5a19f3198c303758
engineerStuardo <ingeniero.stuardo@gmail.com>
2023-06-15 18:33:17 -0600

    Initial Commit

---
+++ NetworkInterchangable/NetworkInterchangableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangableApp.swift
+//  NetworkInterchangable
+//
+//  Created by Italo Stuardo on 15/6/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
